75d55757a82c6ec553d5b81652020fb37b5edef8
PITBULL <rosolovskiy2005@gmail.com>
2022-03-11 12:10:24 +0200

    apdate components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+let test = [1,2,3,6,7,8,9,0];
+
+
+function pop(arr) {
+    arr.length = arr.length - 1;  
+    console.log(arr);
+}
+
+pop(test);
+pop(test);
+pop(test);
+pop(test);
+pop(test);
